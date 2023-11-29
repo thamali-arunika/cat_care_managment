45c0813103ddb325dea26bf6f902e5647f2f3550
thamali-arunika <130566302+thamali-arunika@users.noreply.github.com>
2023-11-29 15:26:50 +0530

    create home page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HomePageComponent } from "./app/home-page/home-page.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [CommonModule, RouterOutlet, HomePageComponent]
 })
 export class AppComponent {
   title = 'cat-home';
