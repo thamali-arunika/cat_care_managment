b713d6ca324e7566baeced70959f77128d76a70d
thamali-arunika <130566302+thamali-arunika@users.noreply.github.com>
2023-12-16 13:28:47 +0530

    commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { LoginformComponent } from './app/loginform/loginform.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'login',
+        component:LoginformComponent
+    }
+];
---
+++ src/app/app/loginform/loginform.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-loginform',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './loginform.component.html',
+  styleUrl: './loginform.component.css'
+})
+export class LoginformComponent {
+
+}
