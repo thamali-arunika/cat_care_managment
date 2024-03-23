6a390e7a131c501853c18dcaeea5a495f1a17aa3
thamali-arunika <130566302+thamali-arunika@users.noreply.github.com>
2024-03-23 13:33:23 +0530

    remove all for change projcect framework

--- src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { LoginformComponent } from './app/loginform/loginform.component';
-
-export const routes: Routes = [
-    {
-        path: 'login',
-        component:LoginformComponent
-    }
-];
